388846f8e4e266299043a0d631d938b50308edc3
sima49 <sima49@msn.com>
2018-01-02 16:50:06 +0000

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return (object) array(
+            'host' => 'localhost',
+            'dbname' => 'DBNAME',
+            'user' => 'DB_USER',
+            'pass' => 'DB_PASS',
+            'siteBase' => '',
+            'api' => 'AWIN API KEY',
+            'awinUser' => 'AWIN USER ID'
+);
